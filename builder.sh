096408093d41bcf5a0cc5649875748dcd2ca5dce
Chris Forno <jekor@jekor.com>
2020-06-19 00:20:04 +0800

    Self-host jQuery.

--- builder.sh
+++ builder.sh
@@ -8,3 +8,5 @@ popd
 mv src/www $out
 wrapProgram "$out/post-comment" --prefix PATH : "$bash/bin:$findutils/bin:$gnugrep/bin:$coreutils/bin:$jcoreutils/bin:$jsonwrench/bin"
 wrapProgram "$out/gressgraph/graph/POST" --prefix PATH : "$bash/bin:$coreutils/bin:$graphviz/bin:$gressgraph/bin"
+mkdir -p $out/script/jquery
+cp $jquery $out/script/jquery/application.javascript
--- default.nix
+++ default.nix
@@ -1,11 +1,14 @@
-{ stdenv, bash, coreutils, findutils, gnugrep, graphviz, pandoc, makeWrapper
-, jcoreutils, jigplate, jsonwrench, gressgraph
-}:
+{ stdenv, fetchurl, bash, coreutils, findutils, gnugrep, graphviz, pandoc, makeWrapper, jcoreutils, jigplate, jsonwrench, gressgraph }:
 
-stdenv.mkDerivation {
+let
+  jquery = fetchurl {
+    url = http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js;
+    sha256 = "1d000mm39r2jr1sa44jwkbig8hd2japjfkgangamm05nik78vdj7";
+  };
+in stdenv.mkDerivation {
   name = "jekor.com";
   src = ./.;
   buildInputs = [ bash pandoc makeWrapper jcoreutils jigplate jsonwrench ];
-  inherit bash coreutils findutils gnugrep graphviz jcoreutils jsonwrench gressgraph;
+  inherit bash coreutils findutils gnugrep graphviz jquery jcoreutils jsonwrench gressgraph;
   builder = ./builder.sh;
 }
